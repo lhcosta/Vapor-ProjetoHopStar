5846553dc2ad100c4fa09f0ce12e92d22957417d
lhcosta <lucashpc91@gmail.com>
2020-07-04 21:59:01 -0300

    Alterando conexão com o banco

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "root",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "ProjetoHopStar"
-    ), as: .psql)
+    app.databases.use(.postgres(url: "DATABASE_URL"), as: .psql)
     
     app.migrations.add(ResponsibleMigration())
     app.migrations.add(PatientMigration())
